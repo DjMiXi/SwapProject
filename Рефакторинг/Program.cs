52cbe52b0f56cc473b160c421268e4d90eda1e71
User <User@K3-512-03>
2017-12-06 19:25:17 +0500

    Добавьте файлы проекта.

---
+++ Рефакторинг/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Рефакторинг
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 10, b = 12;
+
+            SwapInt(ref a, ref b);
+            Console.WriteLine("a={0}, b={1}", a, b);
+            Console.ReadLine();
+        }
+        /// <summary>
+        /// Обмен значений переменных.
+        /// </summary>
+        /// <param name="a">Первая переменная</param>
+        /// <param name="b">Вторая переменная</param>
+        private static void SwapInt(ref int a, ref int b)
+        {
+            int c = a;
+            a = b;
+            b = c;
+        }
+    }
+}
